ff4f6faa1cd7ad6ed9cf9493472e57fde24fe9c7
rgallus1 <roseannegallus@yahoo.com>
2016-12-08 20:12:23 -0800

    added footer totals and completed forms

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var Storeloc = function(storeName, minCustPerHr, maxCustPerHr, avgCookiesPerCust
   this.storeTotCookies = 0;
   allStores.push(this); //this pushes the data above into the all stores array
   this.calcRandCustPerHr = function() {
-    console.log('Im in the random customer calc');
+    //console.log('Im in the random customer calc');
       // This will return random number of customers
     for (var i = 0; i < businessHour.length; i++) {
       this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHr - this.minCustPerHr+1)) + this.minCustPerHr);
@@ -28,16 +28,16 @@ var Storeloc = function(storeName, minCustPerHr, maxCustPerHr, avgCookiesPerCust
   this.calcRandCustPerHr();
 
   this.calcTotalCookiesSoldEachHour = function() {
-    console.log('im in the calculate cookies');
+    //console.log('im in the calculate cookies');
     for (var i = 0; i < businessHour.length; i++) {
       this.totalCookies.push(Math.ceil(this.randomCustPerHour[i] * this.avgCookiesPerCust));
       this.storeTotCookies += this.totalCookies[i];
       //console.log('inside the calculate total cookie', this.storeTotCookies);
-      console.log(this.totalCookies[i]);
+      //console.log(this.totalCookies[i]);
     }
   }
   this.calcTotalCookiesSoldEachHour();
-  console.log('allStores push', allStores[0]);
+  //console.log('allStores push', allStores[0]);
 }
 function storeloc() { //this is outside the constructor
   new Storeloc('1st and Pike', 23, 65, 6.3);
@@ -50,7 +50,7 @@ function storeloc() { //this is outside the constructor
   //this is the header row function
 function headerRow() { //eslint-disable-line
   var cookiesTable = document.getElementById('cookies');
-  console.log('I am in the headerRow function');
+  //console.log('I am in the headerRow function');
 
 //We need to make the header row first  this is the first column//
   var trEl = document.createElement('tr'); //create tr
@@ -72,15 +72,16 @@ function headerRow() { //eslint-disable-line
   cookiesTable.appendChild(trEl); //added the table row to the table
 } //end of header row function
 
-
+//Store row data
 function storeRows() { //eslint-disable-line
   for (var j = 0; j < allStores.length; j++) {//this spins through allstore array to get the store name
     var sumTotCookies = 0;
-    console.log('we are in storeRows')
+    //console.log('we are in storeRows')
     var trEl = document.createElement('tr'); //this creates the row
     var tdEl = document.createElement ('td'); // this creates the data
     tdEl.textContent = allStores[j].storeName; // this indicated the content
     trEl.appendChild(tdEl);
+
     for (var k = 0; k < allStores[j].totalCookies.length; k++) { //this spins through the array of total cookies for eah location
       sumTotCookies += allStores[j].totalCookies[k];
       var tdEc = document.createElement ('td'); //used different variable so I didnt' overwrite it
@@ -89,30 +90,49 @@ function storeRows() { //eslint-disable-line
     }
     var tdEd = document.createElement ('td'); //used different variable so I didn't overwrite it
     tdEd.textContent = sumTotCookies;
-    trEl.appendChild(tdEd);
-    cookiesTable.appendChild(trEl);
-    console.log('at the end of storenames');
+    trEl.appendChild(tdEd);   //moved sumTotCookies to the row
+    cookiesTable.appendChild(trEl); // move the row to the table
   }
 }
 
+//Store footer data
+function footerRows () {
+  var totalOfTotals = 0;
+  console.log(totalOfTotals);
+  var trEl = document.createElement('tr'); //this creates the row
+  var tdEl = document.createElement ('td'); // this creates the data //this creates the data  ???????
+  tdEl.textContent = ('Store Totals');//this indicates the content
+  trEl.appendChild(tdEl); //this appends to the row
+
+  for (var i = 0; i < businessHour.length; i++) {
+    var footerTotal = 0;
+    console.log('first time footer should equal 0', footerTotal);
+
+    for(var j = 0; j < allStores.length; j++) {//this adds the data in total cookies for each store location
+      console.log(allStores[j][i]);
+      footerTotal += allStores[j][i];
+    }
+
+    console.log('footer total', footerTotal)
+    var tdEc = document.createElement ('td'); //used different variable so I didnt' overwrite it
+    tdEc.textContent = footerTotal;
+    trEl.appendChild(tdEc);
+  }
+  totalOfTotals =+ footerTotal;
+  var tdEf = document.createElement ('td');
+  tdEf.textContent = totalOfTotals;
+  trEl.appendChild(tdEf);
+  cookiesTable.appendChild(trEl);
+}
 
 //This is my function declaration for the event handler
 function handlerInfoSubmit(event) {
   event.preventDefault(); //gotta have it for this purpose. prevents page reload on a 'submit' event
-  // console.log('log of the event object', event);
-  // console.log('log of the event.target', event.target);
-  // console.log('log of the event.target.storeName', event.target.storeName.value);
-  // console.log('log of the event.target.minCustPerHr.value', event.target.minCustPerHr.value);
-  // console.log('log of the event.target.maxCustPerHr.value', event.target.maxCustPerHr.value);
-  // console.log('log of the event.target.avgCookiesPerCust.value', event.target.avgCookiesPerCust.value);
-
 
   if (!event.target.storeName.value || !event.target.minCustPerHr.value ||
     !event.target.maxCustPerHr.value || !event.target.avgCookiesPerCust.value) {
     return alert('Fields cannot be empty!');
   }
-
-  cookiesTable.innerHTML = '';
     //variables for all the values coming in for storeName, min, max, avg.
   var formStoreName = event.target.storeName.value;
   var formMinCustPerHr = event.target.minCustPerHr.value;
@@ -124,18 +144,18 @@ function handlerInfoSubmit(event) {
   // console.log(
   new Storeloc(formStoreName, formMinCustPerHr, formMaxCustPerHr, formAvgCookiesPerHr);
 
-  //allStores.innerHTML = '';
+  cookiesTable.innerHTML = '';
 
   headerRow();
   storeRows();
   //footerRows();
 }
-
+//SalmonForm.addEventListener('submit', handlerInfoSubmit);
 //This is where all my calls are made, these are all my actions
 storeloc();
 headerRow();
 storeRows();
-//footerRow();
+footerRows();
 
 //Event Listerner is always listening
 SalmonForm.addEventListener('submit', handlerInfoSubmit);
